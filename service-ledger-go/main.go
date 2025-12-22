6a03fb29ec93a20dc9d3f6fe434d576d9fce2bd9
emmiduh <emmiduh@gmail.com>
2025-12-22 20:11:59 +0200

    update main.go

--- service-ledger-go/main.go
+++ service-ledger-go/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	http.HnaldeFunc("/transaction", handlers.HandlerTransaction)
+	http.HandleFunc("/transaction", handlers.HandleTransaction)
 	log.Println("Ledeger service running on port 4000")
 	log.Fatal(http.ListenAndServe(":4000", nil))
 }

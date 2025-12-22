8b72d3dc60796dac94213e12d6f543ae4eb16e3a
emmiduh <emmiduh@gmail.com>
2025-12-22 22:09:47 +0200

    update Dockerfile fraud.py

--- service-fraud-python/fraud.py
+++ service-fraud-python/fraud.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-app.route("/health", methods=["GET"])
+@app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "ok"}), 200
 
@@ -17,4 +17,4 @@ def check_fraud():
     return jsonify({"fraud": False}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
